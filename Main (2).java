a48f8f9a0f058864f671989a77a353a5db97e1ba
renanmarx26 <101154828+renanmarx26@users.noreply.github.com>
2022-03-13 14:34:35 -0300

    Questão 2
    class Main {
      public static void main(String[] args) {
      Scanner entrada = new Scanner(System.in);
    
      double Placa;
      double AnoCarro;
    
    		System.out.print("Digite a placa do seu veículo:\n");
        Placa = entrada.nextDouble();
        
        System.out.print("Digite o ano do seu carro:\n");
        AnoCarro = entrada.nextDouble();
        
      if (AnoCarro >= 2010){
      System.out.printIn("Carro Velho");
        
      }else if (AnoCarro > 2010 && AnoCarro <= 2021){
      System.out.printIn("Carro Semi Novo");
      
      }else if (AnoCarro = 2022){
      System.out.printIn ("Carro Novo");
      }
    
     if (Placa = a && Placa = e && Placa = i && Placa = o && Placa = u && Placa = A && Placa = E && Placa = I && Placa = O && Placa = U){
    System.out.printIn ("*"); 
    }
       
      }
    }

---
+++ Main (2).java
@@ -0,0 +1,30 @@
+
+class Main {
+  public static void main(String[] args) {
+  Scanner entrada = new Scanner(System.in);
+
+  double Placa;
+  double AnoCarro;
+
+		System.out.print("Digite a placa do seu veículo:\n");
+    Placa = entrada.nextDouble();
+    
+    System.out.print("Digite o ano do seu carro:\n");
+    AnoCarro = entrada.nextDouble();
+    
+  if (AnoCarro >= 2010){
+  System.out.printIn("Carro Velho");
+    
+  }else if (AnoCarro > 2010 && AnoCarro <= 2021){
+  System.out.printIn("Carro Semi Novo");
+  
+  }else if (AnoCarro = 2022){
+  System.out.printIn ("Carro Novo");
+  }
+
+ if (Placa = a && Placa = e && Placa = i && Placa = o && Placa = u && Placa = A && Placa = E && Placa = I && Placa = O && Placa = U){
+System.out.printIn ("*"); 
+}
+   
+  }
+}
